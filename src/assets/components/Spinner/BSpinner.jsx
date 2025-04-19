e2e93eb4914f66fc4ae67fcd35f27fd8a1bc501b
Rigbyyy <cazunigaca@est.utn.ac.cr>
2025-04-19 14:54:48 -0600

    Create BSpinner.jsx
    Creación de el componente Spinner , con el fin de usarlo para la espera de la carga de la APi por el momento.

---
+++ src/assets/components/Spinner/BSpinner.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const BSpinner = ({ size }) => {
+  return (
+    <div className={`spinner spinner-${size}`}>
+    
+    </div>
+  );
+};
+
+export default BSpinner;
