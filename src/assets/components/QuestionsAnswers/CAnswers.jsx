80bf332cea1478c20b97fd1b33c5168975d78b0c
Emmanuel <emmannuell.sanchhez@gmail.com>
2025-04-30 13:02:58 -0600

    Cambio del componente CAnswear
    Se cambia el modelo de botones de bootstrap a css

--- src/assets/components/QuestionsAnswers/CAnswers.jsx
+++ src/assets/components/QuestionsAnswers/CAnswers.jsx
@@ -1,9 +1,19 @@
 function CAnswers({ text, color, onClick }) {
-    return (
-      <a href="#" className={`btn btn-${color} w-100 m-1 p-30px`} onClick={onClick}>
-        {text}
-      </a>
-    );
-  }
-  
-  export default CAnswers;
+  return (
+    <a
+      href="#"
+      className={`botonPreguntas ${
+        color === "success"
+          ? "botonCorrectoPreguntas"
+          : color === "danger"
+          ? "botonIncorrectoPreguntas"
+          : "botonNormalPreguntas"
+      }`}
+      onClick={onClick}
+    >
+      {text}
+    </a>
+  );
+}
+
+export default CAnswers;
